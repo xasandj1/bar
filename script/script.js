a2ba94a90aa3480e44a043d49d6f871b63cc427c
xasan <xasan>
2022-10-05 20:34:07 +0500

    test

---
+++ script/script.js
@@ -0,0 +1,5 @@
+var ism = prompt("ismingiz nima ??")
+var hozir = prompt("hozirgi yili kiriting??")
+var yil = prompt("tugilgan yili kiriting??")
+
+console.log('salom ' + ism +' sizning yoshingiz  ' + (hozir - yil) +' da');
